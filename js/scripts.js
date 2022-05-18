75e771b2bbffd6d6abf380425baad916dec87407
ladegbaye Stella <Ladegbayeewatomi50@gmail.com>
2022-05-18 17:03:53 +0100

    adding my scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,23 @@
-// $(document).ready(function () {
-//   $("#ident").click(function () {
+$(document).ready(function () {
+  $("#ident").click(function () {
 
-// let Deck =[]
-// let suits =[Qeen,King]
-
-
-
-
-//   });
+    let Deck = []
+    let suits = ["diamond", "hearts", "clubs", "spades"]
+    let values = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "king", "queen"]
 
 
 
 
+
+  });
 
 
 
 
 
 
-// });
+
+
+
+
+});
