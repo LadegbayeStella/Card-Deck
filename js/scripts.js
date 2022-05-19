af8214394c8863e3c9f573a9f284f718caea2b41
ladegbaye Stella <Ladegbayeewatomi50@gmail.com>
2022-05-19 04:52:25 +0100

    including my initial style file

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,17 @@
 $(document).ready(function () {
   $("#ident").click(function () {
 
-    let Deck = []
-    let suits = ["diamond", "hearts", "clubs", "spades"]
-    let values = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "king", "queen"]
+    let Deck = [];
+    let suits = ["diamond", "hearts", "clubs", "spades"];
+    let values = ["Ace", "2", "3", "4", "5", "6", "7", "8", "9", "10", "king", "queen"];
 
+    suits.forEach(function (suit) {
+      values.forEach(function (value) {
 
+        Deck.push('  ${value} ${suit} ');
+
+      });
+    });
 
 
 
